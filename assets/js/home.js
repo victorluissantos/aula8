b8724dc8ab522103d4cfd077f1cc95333c2123dd
Santos L Victor <victorluissantos@live.com>
2020-03-12 21:10:43 -0300

    subindo a primeira versao

---
+++ assets/js/home.js
@@ -0,0 +1,17 @@
+//This simple script handles the header nav menu modal
+//also covered nav sub menu selection
+//Dependencies: jQuery
+$(document).ready(function() {
+
+	$('.burger').click(function(){
+		$('header').toggleClass('clicked');
+	});
+
+	$('nav ul li').click(function(){
+		$('nav ul li').removeClass('selected');
+		$('nav ul li').addClass('notselected');
+		$(this).toggleClass('selected');
+		$(this).removeClass('notselected');
+	});
+	
+});
